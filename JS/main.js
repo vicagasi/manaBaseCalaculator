db9508f304e215129197a376426fb590de77c7ea
Victor Gasior <vicagasi@ut.utm.edu>
2023-02-16 10:33:22 -0600

    - added support for  the 'colorless' color

--- JS/main.js
+++ JS/main.js
@@ -6,26 +6,29 @@ function calcAll(){
     b = document.getElementById('black').value;
     w = document.getElementById('white').value;
     g = document.getElementById('green').value;
-    determineLands(l,r,u,b,w,g);
+    c = document.getElementById('green').value;
+    determineLands(l,r,u,b,w,g, c);
 }
 
-function determineLands(lands, red, blue, black, white, green){
-    let totalColor = parseInt(red) + parseInt(blue) + parseInt(black) + parseInt(white) + parseInt(green);
+function determineLands(lands, red, blue, black, white, green, colorless){
+    let totalColor = parseInt(red) + parseInt(blue) + parseInt(black) + parseInt(white) + parseInt(green) + parseInt(colorless);
     let redOut = landForColor(lands, red, totalColor);
     let blueOut = landForColor(lands, blue, totalColor);
     let blackOut = landForColor(lands, black, totalColor);
     let whiteOut = landForColor(lands, white, totalColor);
     let greenOut = landForColor(lands, green, totalColor);
-    setOutput(redOut, blueOut, blackOut, whiteOut, greenOut);
+    let lessOut = landForColor(lands, colorless, totalColor);
+    setOutput(redOut, blueOut, blackOut, whiteOut, greenOut, lessOut);
 }
 
 // Set divs to display the calculations
-function setOutput(red, blue, black, white, green){
+function setOutput(red, blue, black, white, green, colorless){
     document.getElementById("rOut").innerHTML = red;
     document.getElementById("uOut").innerHTML = blue;
     document.getElementById("bOut").innerHTML = black;
     document.getElementById("wOut").innerHTML = white;
     document.getElementById("gOut").innerHTML = green;
+    document.getElementById("lOut").innerHTML = colorless;
 }
 
 // Returns the amount of lands for a specific color
